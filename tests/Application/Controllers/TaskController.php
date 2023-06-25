8b1e98a54ecf32e407d4a6c7b67d92b2ff7a3337
Lucas Eduardo Lima Da Silva <lucaspity721@gmail.com>
2023-06-24 23:23:27 -0300

    Criação de controllers, services e routes para todo list

---
+++ tests/Application/Controllers/TaskController.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class testeTask extends TestCase
+{
+    public function testFindTaskById()
+    {
+        
+
+    }
+}
